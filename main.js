5ac47ca44d9d774653593f7d1d392ee8a8e498a8
zoranaism <lazareviczorana@gmail.com>
2021-07-16 23:11:20 +0200

    created app

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks', 
+            description: 'These socks are awesome.'
         }
     }
 })
